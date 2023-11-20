efaf80b623ed01cbc1392ea64cb13efbf9483756
Kaja11-jain <151500414+Kaja11-jain@users.noreply.github.com>
2023-11-20 13:45:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express=require("express");
+const mongoose=require("mongoose");
+mongoose.connect('mongodb+srv://Kajaljain:607t77vKx1Puh9jx@cluster0.bvczz0u.mongodb.net/Tutorial',
+{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>{
+    console.log("db connection done");
+})
+
+const userroute=require("./route/users")
+const productroute=require("./route/product")
+
+const app=express();
+app.use(express.json());
+
+app.use("/api",userroute)
+app.use("/api",productroute)
+app.listen(2000,()=>{
+    console.log("My server is ready !");
+
+})
